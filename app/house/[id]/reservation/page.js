0c0474451aae3cf438d6bdc275c968110aff4041
M S <mo.sa963@hotmail.com>
2023-05-27 00:26:23 +0300

    Move reservation screen into app routes

--- app/house/[id]/reservation/page.js
+++ app/house/[id]/reservation/page.js
@@ -1,35 +1,35 @@
+"use client"
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import DateInput from "../elements/DateInput";
-import RatingBar from "../elements/RatingBar";
-import { useRequest } from "../contexts/RequestContext";
-import ButtonGradient from "../elements/ButtonGradient";
-import Counter from "../elements/Counter";
-import Text from "../elements/Text";
-import { getHouse } from "./ShowHouse";
+import { getHouse } from "../page";
+import Text from "@/components/elements/Text";
+import RatingBar from "@/components/elements/RatingBar";
+import DateInput from "@/components/elements/DateInput";
+import Counter from "@/components/elements/Counter";
+import ButtonGradient from "@/components/elements/ButtonGradient";
+import request from "@/utils/Request";
 
 
 
 const Reservation = () => {
-    const { search } = useLocation();
+    const [data, setData] = React.useState({});
     const [house, setHouse] = React.useState(null);
     const [errors, setErrors] = React.useState(null);
-    const request = useRequest();
-    const quary = React.useMemo(() => new URLSearchParams(search), [search]);
-    const params = useParams();
-    const [data, setData] = React.useState({});
     const [processing, setProcessing] = React.useState(false);
-    const nav = useNavigate();
+    const params = useParams();
+    const search = useSearchParams();
+    const router = useRouter();
 
+    
     React.useEffect(()=>{
         setData(v => ({
             ...v,
-            start_date: quary.get("in"),
-            end_date: quary.get("out"),
-            guests: quary.get("guests"),
+            start_date: search.get("in"),
+            end_date: search.get("out"),
+            guests: search.get("guests"),
         }));
-        getHouse(request, params.id, setHouse);
-    }, [params.id, quary, request]);
+        getHouse(params.id, setHouse);
+    }, [params.id, search]);
 
     const handleInput = (key, value) => {
         data[key] = value;
@@ -37,13 +37,13 @@ const Reservation = () => {
     }
 
     const handleClick = ()=>{
-        createContract(request, house.id, data, nav, setErrors, setProcessing);
+        createContract(house.id, data, router, setErrors, setProcessing);
     }
 
     return (
         <div className="w-full grid grid-cols-1 bg-inherit mt-24 max-w-5xl px-2 gap-5">
             {
-                errors&&Object.values(errors).map((e, i) => <p key={i}>{e}</p>)
+                errors && Object.values(errors).map((e, i) => <p key={i}>{e}</p>)
             }
             <div className="flex flex-col">
                 <div className="flex flex-wrap">
@@ -67,20 +67,19 @@ const Reservation = () => {
 }
 
 
-const createContract = async (request, house_id, data, nav, setError, setProcessing) => {
+const createContract = async (house_id, data, router, setError, setProcessing) => {
     setProcessing(true);
     const res = await request(`api/house/${house_id}/contract`, "POST", data);
 
     if (res.ok){
         const js = await res.json();
-        nav("/contract/" + js.id);
+        router.push("/contract/" + js.id);
         return;
     }
     const js = await res.json();
 
     setError(js?.errors || js?.message);
     setProcessing(false);
-
 }
 
 export default Reservation;
