eab4679dfbac2e8e973ce5d0e649f13cafc8d0fd
M S <mo.sa963@hotmail.com>
2023-05-28 01:03:21 +0300

    Remove unused prop 'to'

--- components/NavBar/NavButton.jsx
+++ components/NavBar/NavButton.jsx
@@ -5,11 +5,11 @@ import { interpolate } from "@/utils/Interpolator";
 import Link from "next/link";
 
 const NavButton = ({ children, Icon, title, href }) => {
-    const {scroll} = useScroll();
-    const {size} = useScreenSize();
+    const { scroll } = useScroll();
+    const { size } = useScreenSize();
 
     return (
-        <Link href={href} title={title} className="aspect-auto sm:aspect-square hover:scale-105 w-full h-fit sm:h-full sm:w-fit" style={{ padding: interpolate(scroll, [0, 150], [3, 1]) + "px" }} to="">
+        <Link href={href} title={title} className="aspect-auto sm:aspect-square hover:scale-105 w-full h-fit sm:h-full sm:w-fit" style={{ padding: interpolate(scroll, [0, 150], [3, 1]) + "px" }} >
             <div style={{ borderRadius: size === State.sm ? 0 : interpolate(scroll, [0, 150], [50, 0]) + "%" }} 
                 className="h-full w-full justify-center items-center flex cursor-pointer overflow-hidden bg-slate-500 bg-opacity-10 transition-colors hover:bg-opacity-50">
                 <div className="w-full h-full flex justify-start items-center sm:justify-center p-2 px-7 sm:p-0 " >
