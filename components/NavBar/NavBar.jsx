d1ec2f765160ac6fc857b2aba64ae196ddf9e8fa
M S <mo.sa963@hotmail.com>
2023-05-28 01:09:55 +0300

    Add all NavButtons to ToolsBar

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -2,26 +2,12 @@ import React from "react";
 import NavBarBase from "./NavBarBase";
 import Link from "next/link";
 import AppIcon from "../AppIcon";
-import {BsList} from "react-icons/bs";
-import { State, useScreenSize } from "@/contexts/ScreenContext";
-import { useTheme } from "@/contexts/ThemeContext";
-import NavButton from "./NavButton";
 import ToolsBar from "./ToolsBar";
-import {AiOutlineHome} from "react-icons/ai";
-import {IoMdAdd} from "react-icons/io";
-import {VscListSelection} from "react-icons/vsc";
-import { useRouter } from "next/navigation";
-import AuthNavItem from "./AuthNavItem";
 import ThemeButton from "./ThemeButton";
 
 
 const NavBar = () => {
-    const [collapse, setCollapse] = React.useState(false);
-    const { size } = useScreenSize();
 
-    React.useEffect(()=>{
-        setCollapse(size !== State.sm);
-    }, [size]);
 
     return (
         <NavBarBase>
@@ -31,16 +17,7 @@ const NavBar = () => {
 
             <ThemeButton />
 
-            <div className="h-full aspect-square justify-center items-center flex sm:hidden cursor-pointer" onClick={() => setCollapse(!collapse)}>
-                <BsList className="w-6 h-6"/>
-            </div>
-
-            <ToolsBar collapse={collapse} size={size}>
-                <NavButton Icon={AiOutlineHome} title="Home" href="/" />
-                <NavButton Icon={IoMdAdd} title="Host your place" href="/house" />
-                <NavButton Icon={VscListSelection} title="Houses list" href="/house/list" />
-                <AuthNavItem />
-            </ToolsBar>
+            <ToolsBar />
         </NavBarBase>
     );
 }
--- components/NavBar/ToolsBar.jsx
+++ components/NavBar/ToolsBar.jsx
@@ -1,16 +1,36 @@
-import { State } from "@/contexts/ScreenContext";
+"use client"
+import { State, useScreenSize } from "@/contexts/ScreenContext";
 import React from "react";
+import NavButton from "./NavButton";
+import AuthNavItem from "./AuthNavItem";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoMdAdd } from "react-icons/io";
+import { VscListSelection } from "react-icons/vsc";
+import { BsList } from "react-icons/bs";
 
 
 
+const ToolsBar = () => {
+    const [collapse, setCollapse] = React.useState(false);
+    const { size } = useScreenSize();
 
-const ToolsBar = ({ collapse, children, size }) => {
+    React.useEffect(() => {
+        setCollapse(size !== State.sm);
+    }, [size]);
 
-    return(
-        <div style={{ display: (size !== State.sm || collapse) ? "flex" : "none" }}
-            className="flex-shrink-0 flex-wrap sm:bg-transparent w-full sm:w-fit shadow-md sm:shadow-none bg-opacity-30 backdrop-blur-md sm:backdrop-blur-0 items-center h-fit sm:h-full overflow-hidden"  >
-                {children}
-        </div>
+    return (
+        <React.Fragment>
+            <div className="h-full aspect-square justify-center items-center flex sm:hidden cursor-pointer" onClick={() => setCollapse(v => !v)}>
+                <BsList className="w-6 h-6" />
+            </div>
+            <div style={{ display: (size !== State.sm || collapse) ? "flex" : "none" }}
+                className="flex-shrink-0 flex-wrap sm:bg-transparent w-full sm:w-fit shadow-md sm:shadow-none bg-opacity-30 backdrop-blur-md sm:backdrop-blur-0 items-center h-fit sm:h-full overflow-hidden"  >
+                <NavButton Icon={AiOutlineHome} title="Home" href="/" />
+                <NavButton Icon={IoMdAdd} title="Host your place" href="/house" />
+                <NavButton Icon={VscListSelection} title="Houses list" href="/house/list" />
+                <AuthNavItem />
+            </div>
+        </React.Fragment>
     );
 }
 
