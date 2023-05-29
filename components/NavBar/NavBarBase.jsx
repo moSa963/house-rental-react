bfb60abb6879d8f3c708fdbeafca8db4b106f49e
M S <mo.sa963@hotmail.com>
2023-05-29 22:20:24 +0300

    Update navBars shadow

--- components/NavBar/NavBarBase.jsx
+++ components/NavBar/NavBarBase.jsx
@@ -8,11 +8,11 @@ import React from "react";
 const NavBarBase = ({ children }) => {
     const {scroll} = useScroll();
 
-    const p = interpolate(scroll, [0, 150], [15, 0]) + "px";
+    const p = interpolate(scroll, [0, 150], [15, 0]);
 
     return (
-        <div className={`fixed top-0 w-full z-10 select-none`} style={{ paddingLeft: p, paddingTop: p, paddingRight: p, }}>
-            <div className="bg-level3 w-full h-11 rounded-lg shadow-sm px-5 flex flex-wrap" style={{ borderRadius: p}}>
+        <div className={`fixed top-0 w-full z-10 select-none`} style={{ paddingLeft: p + "px", paddingTop: p + "px", paddingRight: p + "px", }}>
+            <div className={`bg-level3 w-full h-11 rounded-lg ${p == 0 ? "shadow-md" : "shadow-sm"} px-5 flex flex-wrap`} style={{ borderRadius: p + "px"}}>
                 { children }
             </div>
         </div>
